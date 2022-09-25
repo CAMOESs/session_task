ae08367b52f87828a9f666632775a957f09247aa
camoes <cam@camoes.fr>
2022-09-25 08:52:58 +0100

    correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    add_flash_types :success, :danger
+    before_action :onlySingnIn
+    helper_method :currentUser, :userSingnIn
+
+    private
+
+    def onlySingnIn
+        if !userSingnIn
+            redirect_to new_user_path, danger: "pas le droit"
+        end 
+    end
+
+    def currentUser
+        return nil if !session[:auth] || !session[:auth]['id']
+        return @_user if @_user
+        @_user = User.find_by_id(session[:auth]['id'])
+    end
+
+    def userSingnIn
+        !currentUser.nil?
+    end
+
 end
