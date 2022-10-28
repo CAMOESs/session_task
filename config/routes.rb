dac7becdc2d445fcb69c38f48636ab79d8507bd2
camoes <camoes@github.com>
2022-10-28 12:56:46 +0100

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  #get '/users/edit', to: 'users#edit', as: :profil
+  get '/users/edit', to: 'users#edit', as: :profil
   #get '/profil', to: 'users#show', as: :show_profil
-  #patch '/users/edit', to: 'users#update'
+  patch '/users/edit', to: 'users#update'
   resources :tasks
   resources :users
-  #get '/users/show', to: 'users#show', as: :show
-  #delete '/sessions/new', to: 'users#destroy', as: :destroy_user
+  #get '/users/show', to: 'users#show', as: :users
+  delete '/sessions/new', to: 'users#destroy', as: :destroy_user
   #session
-  #get '/sessions/new', to: 'sessions#new', as: :new_session
-  #post '/sessions/new', to: 'sessions#create'
-  #get '/sessions/destroy', to: 'sessions#destroy', as: :destroy_session
+  get '/sessions/new', to: 'sessions#new', as: :new_session
+  post '/sessions/new', to: 'sessions#create'
+  get '/sessions/destroy', to: 'sessions#destroy', as: :destroy_session
 end
